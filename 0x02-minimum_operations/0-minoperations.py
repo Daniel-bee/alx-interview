787c63e268444e7c1210442ee2c9a6bc0a5cce3b
Daniel-bee <daniel123_86@live.com>
2022-04-19 17:57:24 +0300

    min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,8 +9,6 @@ def minOperations(n):
     calculates the fewest number of operations needed
     to result in exactly n H characters in the file.
     """
-    if n == 1:
-        return n
     minOp = []
     for x in range(2, int((n / 2) + 1)):
         for y in range(1, int((n / 2) + 1)):
