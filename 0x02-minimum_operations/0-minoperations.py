ab3e19b7dc5dee53595abd76c02494da738c0c8e
Daniel-bee <daniel123_86@live.com>
2022-04-19 17:48:56 +0300

    min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,26 +4,11 @@ Minimum Operations
 """
 
 
-def isPrime(num):
-    """
-    return True if the number is prime
-    """
-    flag = True
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                flag = False
-                break
-    return flag
-
-
 def minOperations(n):
     """
     calculates the fewest number of operations needed
     to result in exactly n H characters in the file.
     """
-    if n <= 0:
-        return 0
     minOp = []
     for x in range(2, int((n / 2) + 1)):
         for y in range(1, int((n / 2) + 1)):
