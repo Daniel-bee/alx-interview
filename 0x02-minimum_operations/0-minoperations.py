93cab24875915402077760f06eea1d96d58d997a
Daniel-bee <daniel123_86@live.com>
2022-04-20 23:10:20 +0300

    min op

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,7 +10,7 @@ def minOperations(n):
     to result in exactly n H characters in the file.
     """
     minOp = 0
-    for x in range(2, (n + 1)):
+    for x in range(2, n):
         while n % x == 0:
             minOp += x
             n /= x
