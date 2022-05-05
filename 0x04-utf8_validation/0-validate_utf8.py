c9c1b13f6d3f3d20e61115e67586696716bf38a6
Daniel-bee <daniel123_86@live.com>
2022-05-05 16:55:45 +0300

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -8,7 +8,7 @@ def validUTF8(data):
     """
     flag = 1
     for num in data:
-        if (num & 255) == 0:
+        if (num & 255) == 0 or num < 0:
             flag = 0
             break
     if flag:
