4636adf7837286aef4829407aad9efc48da23890
Daniel-bee <daniel123_86@live.com>
2022-05-05 17:09:09 +0300

    utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,11 +6,19 @@ def validUTF8(data):
     """
     determines if a given data set represents a valid UTF-8 encoding.
     """
-    flag = 1
-    for num in data:
-        if (num & 255) == 0 or num < 0:
-            flag = 0
-            break
-    if flag:
-        return True
-    return False
+    count = 0
+    for char in data:
+        if count == 0:
+            if (char >> 5) == 0b110:
+                count = 1
+            elif (char >> 4) == 0b1110:
+                count = 2
+            elif (char >> 3) == 0b11110:
+                count = 3
+            elif (char >> 7):
+                return False
+        else:
+            if (char >> 6) != 0b10:
+                return False
+            count -= 1
+    return True
